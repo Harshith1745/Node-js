8f61b77f404e833c743d7b38d8206cd907d85290
Harshith V R <harshithvr352@gmail.com>
2024-09-01 17:56:39 +0530

    added /api/users/:id

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ app.get("/users", (req, res) => {
     const html = `<ul> ${users.map((user) =>
         `<li> ${user.first_name}</li>`).join('')}   </ul>`;
 
-    res.send(html);
+    return res.send(html);
 })
+
+app.get("/api/users/:id",(req,res)=> {
+const id = Number(req.params.id);
+const user = users.find( (user)=> user.id === id);
+  return res.json(user) ;
+})
+
 app.listen(PORT, () => {
     console.log("server listening at PORT:" + PORT);
 })
